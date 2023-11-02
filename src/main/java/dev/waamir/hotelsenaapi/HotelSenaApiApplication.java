3adaf4614918f2122784fb8b4fe0efb3acd39f55
waamir104 <waamirdev@gmail.com>
2023-11-02 11:14:49 -0500

    Initializing the HOTEL SENA API project

---
+++ src/main/java/dev/waamir/hotelsenaapi/HotelSenaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.waamir.hotelsenaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSenaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSenaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/waamir/hotelsenaapi/HotelSenaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.waamir.hotelsenaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSenaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
