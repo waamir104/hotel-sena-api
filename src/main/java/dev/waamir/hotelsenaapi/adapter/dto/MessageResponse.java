df337b22270ae7a89abb18d89ebc685866958162
waamir104 <waamirdev@gmail.com>
2023-12-12 23:08:04 -0400

    Adding the MessageResponse file

---
+++ src/main/java/dev/waamir/hotelsenaapi/adapter/dto/MessageResponse.java
@@ -0,0 +1,14 @@
+package dev.waamir.hotelsenaapi.adapter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageResponse {
+    private String message;
+}
