66e8b3d8a28aaefa2f830aa8146a7a6d0f49a02c
waamir104 <waamirdev@gmail.com>
2023-12-12 23:43:45 -0400

    Modifying the role repository port

--- src/main/java/dev/waamir/hotelsenaapi/domain/port/IRoleRepository.java
+++ src/main/java/dev/waamir/hotelsenaapi/domain/port/IRoleRepository.java
@@ -6,12 +6,13 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import dev.waamir.hotelsenaapi.domain.enumeration.RoleName;
 import dev.waamir.hotelsenaapi.domain.model.Role;
 
 public interface IRoleRepository<T extends Role> {
     T create(T role);
     Optional<T> getById(String id);
-    Optional<T> getByName(String name);
+    Optional<T> getByName(RoleName name);
     void delete(T role);
     void update(T role);
     Page<T> paginate(Pageable pagination);
