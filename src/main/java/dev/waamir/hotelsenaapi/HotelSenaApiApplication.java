db3ff782b2925dbeb5f5011cf00d04c532d8dbc7
waamir104 <waamirdev@gmail.com>
2023-12-16 23:46:32 -0400

    Ading the transaction configuration

--- src/main/java/dev/waamir/hotelsenaapi/HotelSenaApiApplication.java
+++ src/main/java/dev/waamir/hotelsenaapi/HotelSenaApiApplication.java
@@ -2,8 +2,12 @@ package dev.waamir.hotelsenaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import dev.waamir.hotelsenaapi.infrastructure.rest.spring.config.TransactionConfig;
 
 @SpringBootApplication
+@Import(TransactionConfig.class)
 public class HotelSenaApiApplication {
 
 	public static void main(String[] args) {
