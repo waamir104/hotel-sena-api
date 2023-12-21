e703ce0a12ffbd31b7a510008f0f409ed9a9a742
waamir104 <waamirdev@gmail.com>
2023-12-20 21:15:45 -0400

    Removing unnecessary configuration for the transactions with the db

--- src/main/java/dev/waamir/hotelsenaapi/HotelSenaApiApplication.java
+++ src/main/java/dev/waamir/hotelsenaapi/HotelSenaApiApplication.java
@@ -2,12 +2,8 @@ package dev.waamir.hotelsenaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import dev.waamir.hotelsenaapi.infrastructure.rest.spring.config.TransactionConfig;
 
 @SpringBootApplication
-@Import(TransactionConfig.class)
 public class HotelSenaApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/dev/waamir/hotelsenaapi/infrastructure/rest/spring/config/TransactionConfig.java
+++
@@ -1,9 +0,0 @@
-package dev.waamir.hotelsenaapi.infrastructure.rest.spring.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-@EnableTransactionManagement
-public class TransactionConfig {
-}
